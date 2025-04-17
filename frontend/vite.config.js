08ede823de5f070947cefabdbeb318e6c076b624
Sankalp Ghodeswar <sankalpghodeswar@gmail.com>
2025-04-17 11:08:12 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    hot: true,  // Enable Hot Module Replacement (HMR)
-    watch: {
-      usePolling: true, // Use polling to detect file changes
-      interval: 100, // Set polling interval
-    },
-  },
   build: {
-    minify: "terser", // Use Terser for minification
-    sourcemap: true, // Generate source maps for debugging
-  },
+    outDir: 'dist',
+  }
 });
